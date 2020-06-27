b301076135c228945158fcb3c2ca6fa6353982ca
toowoot <1539794351@qq.com>
2020-06-27 23:54:57 +0800

    commit message

---
+++ Lunch.Client/Controllers/HomeController.cs
@@ -0,0 +1,96 @@
+﻿
+
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Lunch.Client.Models;
+using Microsoft.AspNetCore.Authentication;
+using Lunch.Service;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using System.Security.Claims;
+using System.Linq;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Lunch.Client.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly UserService _userService;
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger, UserService userService)
+        {
+            _logger = logger;
+            _userService = userService;
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(string userName, string password)
+        {
+            var user = _userService.Login(userName, _userService.Encrypt(password));
+            if (user == null) return Ok(0);
+
+            var identity = new ClaimsIdentity(CookieAuthenticationDefaults.AuthenticationScheme);
+            identity.AddClaim(new Claim(ClaimTypes.Name, user.Name));
+            identity.AddClaim(new Claim("UserId", user.Id.ToString()));
+            identity.AddClaim(new Claim(ClaimTypes.Role, user.RoleId.ToString()));
+            HttpContext.SignInAsync(new ClaimsPrincipal(identity));
+
+            return Ok(1);
+        }
+
+        public IActionResult Logout()
+        {
+            HttpContext.SignOutAsync();
+            return Redirect("/Home/Login");
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(string username, string password, string address, string phone)
+        {
+            var result = _userService.Register(
+                username, 
+                _userService.Encrypt(password), 
+                address,
+                phone, 
+                2);
+            return Ok(result);
+        }
+
+        [Authorize]
+        public IActionResult ChangePassword(string password, string newPassword)
+        {
+            var userIdStr = User.Claims.SingleOrDefault(s => s.Type == "UserId").Value;
+            int.TryParse(userIdStr, out int userId);
+
+            var result = _userService.ChangePassword(
+                userId, 
+                _userService.Encrypt(password), 
+                _userService.Encrypt(newPassword));
+            return Ok(true);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+
+        [Authorize]
+        public IActionResult UpdatePwd()
+        {
+            return View();
+        }
+    }
+}
---
+++ Lunch.Client/Controllers/MainController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Lunch.Service;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lunch.Client.Controllers
+{
+    public class MainController : Controller
+    {
+        private readonly FoodService _service;
+        private readonly OrderService _orderService;
+
+        public MainController(FoodService service, OrderService orderService)
+        {
+            _service = service;
+            _orderService = orderService;
+        }
+
+        public IActionResult FoodPage()
+        {
+            return View();
+        }
+
+        public IActionResult GetFoods(string keyword, string type, int pageSize, int pageIndex)
+        {
+            var foods = _service.QueryFoods(keyword, type, pageSize, pageIndex, out int total);
+            return Ok(new { Result = true, List = foods, Total = total });
+        }
+
+        public IActionResult ViewImge(int id)
+        {
+            var food = _service.GetFood(id);
+            if (food == null || food.Image.Length == 0) return Content("Error");
+
+            MemoryStream ms = new MemoryStream(food.Image);
+            return new FileStreamResult(ms, new Microsoft.Net.Http.Headers.MediaTypeHeaderValue(food.ImageType));
+        }
+
+        [Authorize]
+        [HttpPost]
+        public IActionResult OrderingFood(int totalPrice, [FromBody]List<int> foodIds)
+        {
+            if (foodIds.Count == 0) return Ok(new { Result = false });
+
+            var userIdStr = User.Claims.SingleOrDefault(s => s.Type == "UserId").Value;
+            int.TryParse(userIdStr, out int userId);
+            if (userId == 0) return Ok(new { Result = false });
+
+            var food = _orderService.OrderingFood(userId, totalPrice, foodIds);
+            return  Ok(new { Result = food });
+        }
+    }
+}
